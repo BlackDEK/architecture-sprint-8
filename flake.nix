03c0352be45973ed85f9235253e44d931be37cc0
BlackDEK <BlackDEK546@yandex.ru>
2025-03-15 15:49:15 +0500

    - Добавил nix для удобства разработки.

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flakeUtils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flakeUtils
+    }:
+    flakeUtils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+        ];
+      };
+
+      shellInput = with pkgs; [
+        # nix
+        nixpkgs-fmt
+        nil
+        statix
+        deadnix
+
+        # pyhton
+        (python311.withPackages (ps: with ps; [
+          autopep8
+        ]))
+      ];
+
+      shell = pkgs.mkShell rec {
+        packages = shellInput;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
+
+        shellHook = ''
+        '';
+      };
+    in
+    {
+      devShells.default = shell;
+    }
+    );
+}
